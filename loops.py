dc616588d7065fbc5b7c14257139d408cda12eb6
Samsung <mukaddesakdu00@gmail.com>
2021-09-05 19:16:43 +0300

    loops eklendi

---
+++ loops.py
@@ -0,0 +1,6 @@
+#itrayt etmek demek, o datayı dönmek demektir.
+
+sehirler = ["Ankara" , "Muğla" , "Kayseri" , "Antalya"]
+
+for sehir in sehirler:             #şehirlerin içinde dolaş demektir.
+    print(sehir)
